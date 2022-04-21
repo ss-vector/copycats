18664236f5b37b411d2f9d2e001ce7649d5b840e
ss-vector <103541253+ss-vector@users.noreply.github.com>
2022-04-21 16:50:45 -0500

    Add files via upload

---
+++ 404.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?>
+
+<div id="primary" class="container">
+	<main id="main" class="site-main">
+
+		<section class="error-404 not-found">
+			<span><h2>Sorry, page not fund!</h2></span>
+		</section>
+
+	</main>
+</div>
+
+<?php get_footer(); ?>
