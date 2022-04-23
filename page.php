b420dac43f1b7b5b4c41ef51fb655f85bde11dbc
ss-vector <omar_mejia@hotmail.com>
2022-04-23 09:37:42 -0500

    update in page.php

--- page.php
+++ page.php
@@ -10,7 +10,18 @@ get_header();
 ?>
 
 
-<h1><?php bloginfo( 'title' ); ?></h1>
+<div class="container">
+    <div class="row">
+        <div class="col-lg-6">
+            <div class="section-wrapper">
+                <article>
+                    <h1><?php echo bloginfo( 'title' ); ?></h1>
+                    <p>Sometimes we need text to fill our test.</p>
+                </article>
+            </div>
+        </div>
+    </div>
+</div>
 
 
 <?php get_footer(); ?>
