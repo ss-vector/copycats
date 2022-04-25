4bf32c5f8d6edf30dc5f2293aaa90f5afb78e831
Omar1992 <omar_mejia@hotmail.com>
2022-04-25 14:09:57 -0500

    updated walker class

--- inc/template/layout/walker.php
+++ inc/template/layout/walker.php
@@ -1,6 +1,20 @@
 <?php
+/**
+ * Nav Menu API: Walker_Nav_Menu class
+ *
+ * @package WordPress
+ * @subpackage Nav_Menus
+ * @since 4.6.0
+ */
 
-class copycats_walker_menu extends Walker {
+/**
+ * Core class used to implement an HTML list of nav menu items.
+ *
+ * @since 3.0.0
+ *
+ * @see Walker
+ */
+class Walker_Nav_Menu extends Walker {
     /**
      * What the class handles.
      *
@@ -10,13 +24,13 @@ class copycats_walker_menu extends Walker {
      * @see Walker::$tree_type
      */
     public $tree_type = array( 'post_type', 'taxonomy', 'custom' );
- 
+
     /**
      * Database fields to use.
      *
      * @since 3.0.0
      * @todo Decouple this.
-     * @var array
+     * @var string[]
      *
      * @see Walker::$db_fields
      */
@@ -24,7 +38,7 @@ class copycats_walker_menu extends Walker {
         'parent' => 'menu_item_parent',
         'id'     => 'db_id',
     );
- 
+
     /**
      * Starts the list before the elements are added.
      *
@@ -45,10 +59,10 @@ class copycats_walker_menu extends Walker {
             $n = "\n";
         }
         $indent = str_repeat( $t, $depth );
- 
-        // Default sub-menu class.
-        $classes = array( 'dropdown-menu' );
- 
+
+        // Default class.
+        $classes = array( 'sub-menu' );
+
         /**
          * Filters the CSS class(es) applied to a menu list element.
          *
@@ -60,10 +74,10 @@ class copycats_walker_menu extends Walker {
          */
         $class_names = implode( ' ', apply_filters( 'nav_menu_submenu_css_class', $classes, $args, $depth ) );
         $class_names = $class_names ? ' class="' . esc_attr( $class_names ) . '"' : '';
- 
+
         $output .= "{$n}{$indent}<ul$class_names>{$n}";
     }
- 
+
     /**
      * Ends the list of after the elements are added.
      *
@@ -86,22 +100,27 @@ class copycats_walker_menu extends Walker {
         $indent  = str_repeat( $t, $depth );
         $output .= "$indent</ul>{$n}";
     }
- 
+
     /**
      * Starts the element output.
      *
      * @since 3.0.0
      * @since 4.4.0 The {@see 'nav_menu_item_args'} filter was added.
+     * @since 5.9.0 Renamed `$item` to `$data_object` and `$id` to `$current_object_id`
+     *              to match parent class for PHP 8 named parameter support.
      *
      * @see Walker::start_el()
      *
-     * @param string   $output Used to append additional content (passed by reference).
-     * @param WP_Post  $item   Menu item data object.
-     * @param int      $depth  Depth of menu item. Used for padding.
-     * @param stdClass $args   An object of wp_nav_menu() arguments.
-     * @param int      $id     Current item ID.
+     * @param string   $output            Used to append additional content (passed by reference).
+     * @param WP_Post  $data_object       Menu item data object.
+     * @param int      $depth             Depth of menu item. Used for padding.
+     * @param stdClass $args              An object of wp_nav_menu() arguments.
+     * @param int      $current_object_id Optional. ID of the current menu item. Default 0.
      */
-    public function start_el( &$output, $item, $depth = 0, $args = null, $id = 0 ) {
+    public function start_el( &$output, $data_object, $depth = 0, $args = null, $current_object_id = 0 ) {
+        // Restores the more descriptive, specific name for use within this method.
+        $menu_item = $data_object;
+
         if ( isset( $args->item_spacing ) && 'discard' === $args->item_spacing ) {
             $t = '';
             $n = '';
@@ -110,66 +129,62 @@ class copycats_walker_menu extends Walker {
             $n = "\n";
         }
         $indent = ( $depth ) ? str_repeat( $t, $depth ) : '';
- 
-        $classes   = empty( $item->classes ) ? array() : (array) $item->classes;
-        $classes[] = 'menu-item-' . $item->ID;
-        $classes = array();
-        $classes[] = 'nav-item';
- 
+
+        $classes   = empty( $menu_item->classes ) ? array() : (array) $menu_item->classes;
+        $classes[] = 'menu-item-' . $menu_item->ID;
+
         /**
          * Filters the arguments for a single nav menu item.
          *
          * @since 4.4.0
          *
-         * @param stdClass $args  An object of wp_nav_menu() arguments.
-         * @param WP_Post  $item  Menu item data object.
-         * @param int      $depth Depth of menu item. Used for padding.
+         * @param stdClass $args      An object of wp_nav_menu() arguments.
+         * @param WP_Post  $menu_item Menu item data object.
+         * @param int      $depth     Depth of menu item. Used for padding.
          */
-        $args = apply_filters( 'nav_menu_item_args', $args, $item, $depth );
- 
+        $args = apply_filters( 'nav_menu_item_args', $args, $menu_item, $depth );
+
         /**
          * Filters the CSS classes applied to a menu item's list item element.
          *
          * @since 3.0.0
          * @since 4.1.0 The `$depth` parameter was added.
          *
-         * @param string[] $classes Array of the CSS classes that are applied to the menu item's `<li>` element.
-         * @param WP_Post  $item    The current menu item.
-         * @param stdClass $args    An object of wp_nav_menu() arguments.
-         * @param int      $depth   Depth of menu item. Used for padding.
+         * @param string[] $classes   Array of the CSS classes that are applied to the menu item's `<li>` element.
+         * @param WP_Post  $menu_item The current menu item object.
+         * @param stdClass $args      An object of wp_nav_menu() arguments.
+         * @param int      $depth     Depth of menu item. Used for padding.
          */
-        $class_names = implode( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item, $args, $depth ) );
+        $class_names = implode( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $menu_item, $args, $depth ) );
         $class_names = $class_names ? ' class="' . esc_attr( $class_names ) . '"' : '';
- 
+
         /**
          * Filters the ID applied to a menu item's list item element.
          *
          * @since 3.0.1
          * @since 4.1.0 The `$depth` parameter was added.
          *
-         * @param string   $menu_id The ID that is applied to the menu item's `<li>` element.
-         * @param WP_Post  $item    The current menu item.
-         * @param stdClass $args    An object of wp_nav_menu() arguments.
-         * @param int      $depth   Depth of menu item. Used for padding.
+         * @param string   $menu_id   The ID that is applied to the menu item's `<li>` element.
+         * @param WP_Post  $menu_item The current menu item.
+         * @param stdClass $args      An object of wp_nav_menu() arguments.
+         * @param int      $depth     Depth of menu item. Used for padding.
          */
-        $id = apply_filters( 'nav_menu_item_id', 'menu-item-' . $item->ID, $item, $args, $depth );
+        $id = apply_filters( 'nav_menu_item_id', 'menu-item-' . $menu_item->ID, $menu_item, $args, $depth );
         $id = $id ? ' id="' . esc_attr( $id ) . '"' : '';
- 
+
         $output .= $indent . '<li' . $id . $class_names . '>';
- 
+
         $atts           = array();
-        $atts['title']  = ! empty( $item->attr_title ) ? $item->attr_title : '';
-        $atts['target'] = ! empty( $item->target ) ? $item->target : '';
-        if ( '_blank' === $item->target && empty( $item->xfn ) ) {
+        $atts['title']  = ! empty( $menu_item->attr_title ) ? $menu_item->attr_title : '';
+        $atts['target'] = ! empty( $menu_item->target ) ? $menu_item->target : '';
+        if ( '_blank' === $menu_item->target && empty( $menu_item->xfn ) ) {
             $atts['rel'] = 'noopener';
         } else {
-            $atts['rel'] = $item->xfn;
+            $atts['rel'] = $menu_item->xfn;
         }
-        $atts['href']         = ! empty( $item->url ) ? $item->url : '';
-        $atts['aria-current'] = $item->current ? 'page' : '';
-        // Add new atribute
-        $atts['class'] = 'nav-link';
- 
+        $atts['href']         = ! empty( $menu_item->url ) ? $menu_item->url : '';
+        $atts['aria-current'] = $menu_item->current ? 'page' : '';
+
         /**
          * Filters the HTML attributes applied to a menu item's anchor element.
          *
@@ -185,12 +200,12 @@ class copycats_walker_menu extends Walker {
          *     @type string $href         The href attribute.
          *     @type string $aria-current The aria-current attribute.
          * }
-         * @param WP_Post  $item  The current menu item.
-         * @param stdClass $args  An object of wp_nav_menu() arguments.
-         * @param int      $depth Depth of menu item. Used for padding.
+         * @param WP_Post  $menu_item The current menu item object.
+         * @param stdClass $args      An object of wp_nav_menu() arguments.
+         * @param int      $depth     Depth of menu item. Used for padding.
          */
-        $atts = apply_filters( 'nav_menu_link_attributes', $atts, $item, $args, $depth );
- 
+        $atts = apply_filters( 'nav_menu_link_attributes', $atts, $menu_item, $args, $depth );
+
         $attributes = '';
         foreach ( $atts as $attr => $value ) {
             if ( is_scalar( $value ) && '' !== $value && false !== $value ) {
@@ -198,28 +213,28 @@ class copycats_walker_menu extends Walker {
                 $attributes .= ' ' . $attr . '="' . $value . '"';
             }
         }
- 
+
         /** This filter is documented in wp-includes/post-template.php */
-        $title = apply_filters( 'the_title', $item->title, $item->ID );
- 
+        $title = apply_filters( 'the_title', $menu_item->title, $menu_item->ID );
+
         /**
          * Filters a menu item's title.
          *
          * @since 4.4.0
          *
-         * @param string   $title The menu item's title.
-         * @param WP_Post  $item  The current menu item.
-         * @param stdClass $args  An object of wp_nav_menu() arguments.
-         * @param int      $depth Depth of menu item. Used for padding.
+         * @param string   $title     The menu item's title.
+         * @param WP_Post  $menu_item The current menu item object.
+         * @param stdClass $args      An object of wp_nav_menu() arguments.
+         * @param int      $depth     Depth of menu item. Used for padding.
          */
-        $title = apply_filters( 'nav_menu_item_title', $title, $item, $args, $depth );
- 
+        $title = apply_filters( 'nav_menu_item_title', $title, $menu_item, $args, $depth );
+
         $item_output  = $args->before;
         $item_output .= '<a' . $attributes . '>';
         $item_output .= $args->link_before . $title . $args->link_after;
         $item_output .= '</a>';
         $item_output .= $args->after;
- 
+
         /**
          * Filters a menu item's starting output.
          *
@@ -230,26 +245,27 @@ class copycats_walker_menu extends Walker {
          * @since 3.0.0
          *
          * @param string   $item_output The menu item's starting HTML output.
-         * @param WP_Post  $item        Menu item data object.
+         * @param WP_Post  $menu_item   Menu item data object.
          * @param int      $depth       Depth of menu item. Used for padding.
          * @param stdClass $args        An object of wp_nav_menu() arguments.
          */
-        $output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
+        $output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $menu_item, $depth, $args );
     }
- 
+
     /**
      * Ends the element output, if needed.
      *
      * @since 3.0.0
+     * @since 5.9.0 Renamed `$item` to `$data_object` to match parent class for PHP 8 named parameter support.
      *
      * @see Walker::end_el()
      *
-     * @param string   $output Used to append additional content (passed by reference).
-     * @param WP_Post  $item   Page data object. Not used.
-     * @param int      $depth  Depth of page. Not Used.
-     * @param stdClass $args   An object of wp_nav_menu() arguments.
+     * @param string   $output      Used to append additional content (passed by reference).
+     * @param WP_Post  $data_object Menu item data object. Not used.
+     * @param int      $depth       Depth of page. Not Used.
+     * @param stdClass $args        An object of wp_nav_menu() arguments.
      */
-    public function end_el( &$output, $item, $depth = 0, $args = null ) {
+    public function end_el( &$output, $data_object, $depth = 0, $args = null ) {
         if ( isset( $args->item_spacing ) && 'discard' === $args->item_spacing ) {
             $t = '';
             $n = '';
@@ -259,5 +275,3 @@ class copycats_walker_menu extends Walker {
         }
         $output .= "</li>{$n}";
     }
- 
-}
