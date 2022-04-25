aeaf0ea671a3a9a22310cc35bd7d47808c63a7c9
Omar1992 <omar_mejia@hotmail.com>
2022-04-25 14:35:18 -0500

    random update

--- inc/partials/header/parts/menu.php
+++ inc/partials/header/parts/menu.php
@@ -4,6 +4,7 @@
 				'theme_location' 	=> 'primary-home-menu',
 				'container'			=> false,
 				'depth'				=> 3,
-				'menu_class'      => 'nav navbar-nav ml-auto',
+				'menu_class'      	=> 'nav navbar-nav ml-auto',
+				'waker'				=> new Walker_Nav_Menu(),
 		));
 ?>
