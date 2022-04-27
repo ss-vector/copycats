c3ebf2c579334bb92451e5e622d35bc4010d3c6c
ss-vector <omar_mejia@hotmail.com>
2022-04-27 12:55:18 -0500

    page.php random update

--- page.php
+++ page.php
@@ -12,7 +12,7 @@ get_header();
 
 <div class="container">
     <div class="row">
-        <div class="col-lg-6">
+        <div class="col-lg-12">
             <div class="section-wrapper">
                 <article>
                     <?php if( have_posts() ):
@@ -20,7 +20,7 @@ get_header();
                                 <h1><?php the_title( sprintf( '<a href="%s"><h2 class="blog-entry-title">', esc_url( get_permalink() ) ), '</h2></a>' ); ?></h1>
                                 <?php the_content(); ?>
                             <?php endwhile; ?>
-                    <?php endif; ?>
+                <?php endif; ?>
                 </article>
             </div>
         </div>
