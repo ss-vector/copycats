e10dd17909243832b9f3d8d00df9bdd0f327bfbd
ss-vector <omar_mejia@hotmail.com>
2022-04-28 23:43:04 -0500

    random update

--- front-page.php
+++ front-page.php
@@ -4,22 +4,21 @@
  * @package Copycats
  */ 
 ?>
-<div class="cover-display">
-	<div class="primary-container">
-	
-		<div class="container">
+
+<div class="container-fluid">
+	<div class="primary-container container">
 		
-			<div class="article-wrapper">
-				<div class="home-featured-content">
-					<article>
-						<h1>Negocios El Triunfo - Lambayeque</h1>
-						<h2>Una comunidad de negocios local</h2>
-						<span></span>
-					</article>
-				</div>
-			</div>
+
+	
+		<div class="article-wrapper">
+			<article>
+				<h1>Negocios El Triunfo - Lambayeque</h1>
+				<h2>Una comunidad de negocios local</h2>
+				<span></span>
+			</article>
 
 		</div>
+
 	</div>
 </div>
 
