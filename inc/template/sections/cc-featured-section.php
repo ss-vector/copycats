58925a02223f2a445d57e6f53907f713e7e8a7c3
ss-vector <omar_mejia@hotmail.com>
2023-05-08 11:44:18 -0500

    added featured section on front-page

--- inc/template/sections/cc-featured-section.php
+++ inc/template/sections/cc-featured-section.php
@@ -1,3 +1,17 @@
-<div>
-	<a href="https://clpe5.com"><span class="fs-4">Club de Lectura</span></a>
+<div class="container">
+
+	<div class="section-title">
+		<h3>Patrocinado</h3>
+	</div>
+	<div>
+		<section class="row">
+			<div class="col-md-3 card align-items-center">			
+				<a href="https://clpe5.com"  target="_blank"><img src="https://user-images.githubusercontent.com/103541253/235826459-e5d5955a-c72f-4ffe-81f3-77c7bb3d9524.svg"></a>
+			<h3 class="fs-3">Club de Lectura para Emprendedores</h3>
+		</div>
+
+		</section>
+		
+	</div>
+
 </div>
