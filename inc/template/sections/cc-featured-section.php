1017aa4003aea2b5362fb63beaa6114f42d38fe4
ss-vector <omar_mejia@hotmail.com>
2023-05-08 14:01:22 -0500

    update

--- inc/template/sections/cc-featured-section.php
+++ inc/template/sections/cc-featured-section.php
@@ -1,12 +1,12 @@
-<div class="container">
+<div class="container-fluid">
 
 	<div class="section-title">
 		<h3>Patrocinado</h3>
 	</div>
-	<div>
+	<div class="container">
 		<section class="row">
-			<div class="col-md-3 card align-items-center">			
-				<a href="https://clpe5.com"  target="_blank"><img src="https://user-images.githubusercontent.com/103541253/235826459-e5d5955a-c72f-4ffe-81f3-77c7bb3d9524.svg"></a>
+			<div class="col-md-2 card align-items-center">			
+				<a href="https://clpe5.com"  target="_blank"><img class="img-thumbnail" src="https://user-images.githubusercontent.com/103541253/235826459-e5d5955a-c72f-4ffe-81f3-77c7bb3d9524.svg"></a>
 			<h3 class="fs-3">Club de Lectura para Emprendedores</h3>
 		</div>
 
