22bf8c542ec0507cb24e6b0870e1de188e4be337
shepperd <fxdark90@gmail.com>
2023-05-08 16:44:35 -0500

    adding featured section brands

--- inc/template/sections/cc-featured-section.php
+++ inc/template/sections/cc-featured-section.php
@@ -6,10 +6,12 @@
 	<div class="container">
 		<section class="row">
 			<div class="col-md-2 card align-items-center">			
-				<a href="https://clpe5.com"  target="_blank"><img class="img-thumbnail" src="https://user-images.githubusercontent.com/103541253/235826459-e5d5955a-c72f-4ffe-81f3-77c7bb3d9524.svg"></a>
-			<h3 class="fs-3">Club de Lectura para Emprendedores</h3>
-		</div>
-
+				<a href="https://clpe5.com"  target="_blank"><img class="img-thumbnail" src="https://user-images.githubusercontent.com/103541253/235826459-e5d5955a-c72f-4ffe-81f3-77c7bb3d9524.svg"></a>
+	  	</div>
+      <!-- TODO: [ ] Define css styles for featured brands container -->
+			<div class="col-md-2 card align-items-center">			
+        <a href="https://clpe5.com"  target="_blank"><img class="img-thumbnail" src="https://user-images.githubusercontent.com/103541253/235826459-e5d5955a-c72f-4ffe-81f3-77c7bb3d9524.svg"></a>
+      </div>
 		</section>
 		
 	</div>
