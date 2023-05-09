20c1142e20c96b44d5343da115d41b1acb5a635a
ss-vector <omar_mejia@hotmail.com>
2023-05-08 23:11:52 -0500

    file structure fixes

--- inc/template/sections/cc-featured-section.php
+++ inc/template/sections/cc-featured-section.php
@@ -7,14 +7,16 @@
 		<section class="row">
 			<div class="col-md-2 card align-items-center">			
         <a href="https://clpe5.com"  target="_blank"><img class="img-thumbnail" src="https://user-images.githubusercontent.com/103541253/235826459-e5d5955a-c72f-4ffe-81f3-77c7bb3d9524.svg"></a>
-        <h3 class="fs-3">CLub de Lectura para<h3/>
+        <h3 class="fs-3">CLub de Lectura para<h3/>
 		</div>
-    <!-- TODO: [ ] Define css styles for featured brands container -->
-			<div class="col-md-2 card align-items-center">			
-				<a href="https://clpe5.com"  target="_blank"><img class="img-thumbnail" src="https://user-images.githubusercontent.com/103541253/235826459-e5d5955a-c72f-4ffe-81f3-77c7bb3d9524.svg"></a>
-			<div class="col-md-2 card align-items-center">			
-        <a href="https://clpe5.com"  target="_blank"><img class="img-thumbnail" src="sstps:/user-images.githubusercontent.com/103541253/235826459-e5d5955a-c72f-4ffe-81f3-77c7bb3d9524.svg"></a>
-      </div>
+    		<!-- TODO: [ ] Define css styles for featured brands container -->
+		<div class="col-md-2 card align-items-center">			
+			<a href="https://clpe5.com"  target="_blank"><img class="img-thumbnail" src="https://user-images.githubusercontent.com/103541253/235826459-e5d5955a-c72f-4ffe-81f3-77c7bb3d9524.svg"></a>
+		</div>
+
+		<div class="col-md-2 card align-items-center">			
+        	<a href="https://clpe5.com"  target="_blank"><img class="img-thumbnail" src="sstps:/user-images.githubusercontent.com/103541253/235826459-e5d5955a-c72f-4ffe-81f3-77c7bb3d9524.svg"></a>
+      	</div>
 		</section>
 		
 	</div>
