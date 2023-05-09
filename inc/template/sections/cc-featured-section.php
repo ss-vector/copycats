827e1de1f6fb3e4116ab43bf2805253550359e77
shepperd <fxdark90@gmail.com>
2023-05-09 10:07:25 -0500

    file update

--- inc/template/sections/cc-featured-section.php
+++ inc/template/sections/cc-featured-section.php
@@ -7,11 +7,12 @@
 		<section class="row">
 			<div class="col-md-2 card align-items-center">			
         <a href="https://clpe5.com"  target="_blank"><img class="img-thumbnail" src="https://user-images.githubusercontent.com/103541253/235826459-e5d5955a-c72f-4ffe-81f3-77c7bb3d9524.svg"></a>
-        <h3 class="fs-3">CLub de Lectura para<h3/>
+        <h3 class="fs-3">Club de Lectura para<h3/>
 		</div>
     		<!-- TODO: [ ] Define css styles for featured brands container -->
 		<div class="col-md-2 card align-items-center">			
-			<a href="https://clpe5.com"  target="_blank"><img class="img-thumbnail" src="https://user-images.githubusercontent.com/103541253/235826459-e5d5955a-c72f-4ffe-81f3-77c7bb3d9524.svg"></a>
+      <a href="https://clpe5.com"  target="_blank"><img class="img-thumbnail" src="https://user-images.githubusercontent.com/103541253/235826459-e5d5955a-c72f-4ffe-81f3-77c7bb3d9524.svg"></a>
+      <h3>Papa Perucha</h3>
 		</div>
 
 		<div class="col-md-2 card align-items-center">			
