b9dd8c36e7e2c5d5bfa9a3bf85991cb4f795214c
shepperd <fxdark90@gmail.com>
2023-06-19 13:24:45 -0500

    adding form section

--- inc/template/sections/cc-newsletter.php
+++ inc/template/sections/cc-newsletter.php
@@ -1,1 +1,11 @@
-cc-newsletter.php
+<?php
+if ($_POST['submit']) {
+    # code
+  } else {
+  # code
+    <form id="formid" action="" method="POST">
+    <input type="text" name="inputname" value="" />
+    <input type="submit" name="submit" value="submit" />
+    </form>  
+  }
+?>
