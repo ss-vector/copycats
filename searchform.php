c20762222e4accf4d49ef58454b5266300a66cb0
ss-vector <omar_mejia@hotmail.com>
2023-07-03 09:17:41 -0500

    working on search-form styles

--- searchform.php
+++ searchform.php
@@ -2,15 +2,21 @@
 	<form role="search" class="navbar-search" method="get" action="<?php echo home_url( '/' ); ?>">
 		<!-- insert if condition here and label -->
 		<div class="input-group">
-			<input 	type="search"
-					class="woo_search__input"
+			<div class="input-search-field">
+				<input 	type="search"
+					class="form-control search-field product-search-field"
 					placeholder="<?php _e('Busque aqui...', 'copycats'); ?>"
 					value="<?php echo get_search_query() ?>"
 					name="s"
 					title="Search"
-			/>
-		    <div class="input-group-addon search-categories" data-category="">
-		        <span><?php esc_attr_e('Todo', 'copycats'); ?></span>
+				/>
+			</div>
+
+		    <div class="input-group-addon search-categories d-flex" data-category="">
+		        <select name="product_cat" id="cc_header_search_categories_dropdown" class="">
+		        	<option value="0" selected="selected">Todas las categorias</option>
+		        	<option class="level-0" value="ferreteria">Ferreteria</option>
+		        </select>
 		    </div>
 		    <div class="input-group-btn">
 				<button type="submit" class="btn btn-primary"><span class="search-btn">Buscar</span></button>
