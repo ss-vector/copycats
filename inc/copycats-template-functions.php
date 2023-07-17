387a24f3675a37edb18d18e1a8452f9585cf8ab7
ss-vector <omar_mejia@hotmail.com>
2023-07-17 00:12:10 -0500

    File renaming

--- inc/copycats-template-functions.php
+++ inc/copycats-template-functions.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * 
+ *
  * @package Copycats
- * 
+ *
  * Copycats Template Functions
  */
-require_once get_template_directory() . '/inc/functions/header.php';
+require_once get_template_directory() . '/inc/functions/header-functions.php';
 require_once get_template_directory() . '/inc/template-tags/home.php';
 
 
