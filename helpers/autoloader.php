1efb326c077950e598fee147fba481977552f0a8
shepperd <fxdark90@gmail.com>
2023-07-18 15:47:56 -0500

    Updated [autoloader.php]

--- helpers/autoloader.php
+++ helpers/autoloader.php
@@ -1,3 +1,24 @@
 <?php
+/**
+ *
+ * Autoloader Function in Theme Setup
+ *
+ * @package Copycats
+ * @version 1.2
+ */
 
+function autolader( $resource = '' ) {
+  $resource_path  = false;
+  $namespace_root = 'COPYCATS_THEME\\'; # ??
+  $resource       = trim( $resource, '\\' );
+
+  if ( empty( $resource ) || strpos( $resource, '\\' ) === false || strpos( $resource, $namespace_root ) !== 0 ) {
+    # not the right namespace, then quit
+    return;
+  }
+https://www.php.net/manual/en/function.str-replace.php
+  $resource = str_replace( $namespace_root, '', $resource ); # str_replace() https://www.php.net/manual/en/function.str-replace.php
+
+  
+}
 ?>
