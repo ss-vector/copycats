bf8abd53842a240a2265305bf9644ac6b825d6d5
shepperd <fxdark90@gmail.com>
2023-07-25 13:23:55 -0500

    reworking [class-menu.php] and [cc-home-menu.php]

--- inc/partials/header/parts/cc-home-menu.php
+++ inc/partials/header/parts/cc-home-menu.php
@@ -12,24 +12,52 @@
 ?>
     <!-- TODO: change id name -->
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
       <?php if ( ! empty( $header_menus ) && is_array() ) {
       ?>
+        
         <ul class="navbar-nav mr-auto">
           <?php
             foreach ( $header_menus as $menu_item ) {
 
-              if ( ! $menu_item->menu_item_prent ) {
-                ?>
+              if ( ! $menu_item->menu_item_parent ) {
+                $child_menu_items = $menu_class->get_child_menu_items( $header_menus, $menu_item->ID );
+                $has_children     = ! empty( $child_menu_items ) && is_array( $child_menu_items );
+                $link_target      = ! empty( $menu_item->target ) && '_blank' === $menu_item->target ? '_blank' : '_self';
 
-                <li class="nav-item">
-                  <a class="nav-link" href="#">New link</a>
-                </li>
-                <li class="nav-item dropdown">
-                  <a class="nav-link dropdown-toggle"
+                if ( ! $has_children ) {
+                  ?>
 
-                </li>
+                  <li class="nav-item">
+                    <a class="nav-link"
+                       href="<?php echo esc_url( $menu_item->url ); ?>">
+   
+                      <?php echo esc_html( $menu_item->title );  ?>
 
-            <?php
+                    </a>
+                  </li>
+
+                <?php 
+                } else {
+                  ?>
+                  
+                  <li class="nav-item dropdown">
+                    <a  class="nav-link dropdown-toggle" 
+                        href="<?php esc_url( $child_menu_item->url ); ?>" 
+                        role="button" 
+                        data-toggle="dropdown"
+                        aria-haspopup="true"
+                        aria-expanded="false"
+                        target="<?php esc_html( $link_target ); ?>"
+                        title="<?php esc_attr( $menu_item->title ); ?>" >
+
+                      <?php esc_html( $menu_item->title ); ?>
+
+                    </a>
+                  </li>
+
+                <?php
+                }
               }
             }
             ?>
