4912507759b3d91b21ae9616123d68f5300adcd4
ss-vector <omar_mejia@hotmail.com>
2023-08-08 19:20:55 -0500

    fixing bugs on [comments.php]

--- comments.php
+++ comments.php
@@ -6,17 +6,44 @@
       <?php
         printf(
           _nx(
-            single: 'Un comentario en "%2$s"',
-            plural: '%1$s comentarios en "%2$s"',
+            'Un comentario en "%2$s"',
+            '%1$s comentarios en "%2$s"',
             get_comments_number(),
-            context: 'comments-title',
-            domain: 'copycats'
+            'comments-title',
+            'copycats'
           ),
-          ...values:number_format_i18n( get_comments_number() ),
-          '<span> . get_the_title() . </span>'
+          number_format_i18n( get_comments_number() ),
+          '<span>' . get_the_title() . '</span>'
         );
       ?>
-
     </h2>
+
+    <ol class="comment-list">
+      <?php
+  			wp_list_comments( array(
+  				'style'       => 'ol',
+  				'short_ping'  => true,
+  				'avatar_size' => 74,
+  			) );
+			?>
+    </ol>
+
+      <?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : ?>
+
+    <nav class="navigation comment-navigation" role="navigation">
+
+      <h1 class="screen-reader-text section-heading"><?php _e( 'Comment navigation', 'copycats' ); ?></h1>
+      <div class="nav-previous"><?php previous_comments_link( __( '&larr; Older Comments', 'copycats' ) ); ?></div>
+      <div class="nav-next"><?php next_comments_link( __( 'Newer Comments &rarr;', 'copycats' ) ); ?></div>
+    </nav>
+      <?php endif; // Check for comment navigation ?>
+
+      <?php if ( ! comments_open() && get_comments_number() ) : ?>
+        <p class="no-comments"><?php _e( 'Comments are closed.', 'aquila' ); ?></p>
+      <?php endif; ?>
+
+    <?php endif; // have_comments() ?>
+
+    <?php comment_form(); ?>
 
 </div>
