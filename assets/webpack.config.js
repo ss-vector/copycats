fc26c0accc1c8c9419b12ee6bc30e0ddf2840167
ss-vector <omar_mejia@hotmail.com>
2023-12-21 11:59:22 -0500

    webpack implementation

---
+++ assets/webpack.config.js
@@ -0,0 +1,20 @@
+const path = require( 'path' );
+// Create paths for each asset
+const JS_DIR = path.resolve( __dirname, '/assets/js' );
+
+
+const entry = {
+  main: JS_DIR + '/main.js',
+  app: JS_DIR + '/app.js',
+};
+const output = {
+  path: BUILD_DIR,
+  filename: 'js/[name].js',
+};
+
+module.exports = ( env, argv );=> ({
+  entry: entry,
+  output: {
+    filename: 'main.js',
+  }
+});
