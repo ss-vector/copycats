dbc329359e02d4fed203bcb6b6b3dc1c26681243
ss-vector <omar_mejia@hotmail.com>
2023-12-21 17:29:44 -0500

    webpack configuration 2

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -1,5 +1,12 @@
+/** Webpack 5 configuration file
+  * @since 1.6.0
+  */
+
 const path = require( 'path' );
-// Create paths for each asset
+const MiniCssExtractPlugin = require( 'mini-css-extract-plugin' );
+
+
+/** Directory path.  TODO: Create paths for each asset */
 const JS_DIR = path.resolve( __dirname, '/assets/js' );
 const BUILD_DIR = path.resolve( __dirname, '/assets/');
 
@@ -13,9 +20,30 @@ const output = {
 };
 
 const rules = [
+  {
+    test: /\.js$/,
+    include:[ JS_DIR ],
+    exclude: /node_modules/,
+    use: 'babel-loader'
+  },
+  {
+    test: /\.scss$/,
+    exclude: /node_modules/,
+    use: [ MiniCssExtractPlugin, 'css-loader' ]
+  },
+  {
+    test: /\.(png|jpe?g|svg|gif|ico)$/,
+    loader: 'file-loader',
+    option: {
+      name: '[path][name].[ext]',
+      // depending on your folder structure.
+      publicPath: 'production' === process.env.NODE_ENV ? '../' : '../../',
+    }
+  }
+
 ];
 
 module.exports = ( env, argv )=> ({
   entry: entry,
-  output: output, 
+  output: output,
 });
