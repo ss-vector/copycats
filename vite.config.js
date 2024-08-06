7125eebb8345532d8a17a08ab33df189e52649d4
ss-vector <omar_mejia@hotmail.com>
2024-08-06 12:27:55 -0500

    vite config file

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  root: 'src', // Directory where your source files are located
-  base: process.env.NODE_ENV === 'production' ? '/wp-content/themes/your-theme/dist/' : '/', // Adjust the production base URL
+  root: 'assets/src', // Directory where your source files are located
+  base: process.env.NODE_ENV === 'production' ? '/wp-content/themes/copycats/dist/' : '/', // Adjust the production base URL
   build: {
     outDir: '../dist', // Directory for build output relative to the config file
     emptyOutDir: true, // Empties the outDir on build
