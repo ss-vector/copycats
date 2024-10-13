7fef855c7f2583c23cc8e37d89b2956d967bec4c
Omar <omar_mejia@hotmail.com>
2024-10-12 22:06:15 -0500

    vite-cat initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,20 @@
+import { resolve } from 'path'; 
 import { defineConfig } from 'vite';
 
 export default defineConfig({
   root: 'assets/src', // Directory where your source files are located
   base: process.env.NODE_ENV === 'production' ? '/wp-content/themes/copycats/dist/' : '/', // Adjust the production base URL
   build: {
-    outDir: '../dist', // Directory for build output relative to the config file
-    emptyOutDir: true, // Empties the outDir on build
+    rollupOptions: {
+      input:{
+        main:           resolve(__dirname,'assets/src/app.js'),
+        copycatsStyles: resolve(__dirname, 'assets/src/css/layot.css') 
+      },
+      external: [jQery],
+      globals: [jQery],
+      output: {
+        entryFileNames: 'assets/js/[name].js',
+      }
+    }
   },
 });
