d91293e0c5d4f395f335d836b5262726dc22fe7c
Omar <omar_mejia@hotmail.com>
2024-10-23 12:06:35 -0500

    disabled woocommerce styles

--- inc/classes/class-woocommerce.php
+++ inc/classes/class-woocommerce.php
@@ -39,6 +39,9 @@ class WooCommerce {
     remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10 );
     remove_action( 'woocommerce_sidebar', 'woocommerce_get_sidebar', 10 );
     remove_action( 'woocommerce_before_shop_loop', 'woocommerce_catalog_ordering', 30 );
+
+  	add_filter( 'woocommerce_enqueue_styles', '__return_false' );
+	
   }
 
   function copycats_woocommerce_custom_content() {
