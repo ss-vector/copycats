3912a8126d56a662d0bc7d3260cd37c2d11e537a
Omar <omar_mejia@hotmail.com>
2025-04-09 21:20:12 -0500

    html content update

--- template/sections/cc-featured-section.php
+++ template/sections/cc-featured-section.php
@@ -1,21 +1,24 @@
 <div class="container-fluid">
 
 	<div class="section-title">
-		<h3>Emprendimientos</h3>
+		<h3>Nuestros Proyectos</h3>
 	</div>
 	<div class="container">
 		<section class="row">
-			<div class="col-md-3 card align-items-center">
-        		<a href="https://clpe5.com"  target="_blank"><img class="img-thumbnail" src="https://user-images.githubusercontent.com/103541253/235826459-e5d5955a-c72f-4ffe-81f3-77c7bb3d9524.svg"></a>
-        		<h3 class="fs-3">Club de Lectura para Emprendedores<h3/>
-			</div>
-    		<!-- TODO: [ ] Define css styles for featured brands container -->
-			<div class="col-md-3 card align-items-center">
-	      		<a href="https://www.facebook.com/profile.php?id=100089558450689"  target="_blank"><img class="img-thumbnail" src="https://eltriunfo.pe/wp-content/uploads/2023/05/papa-1.jpeg"></a>
-            <h3 class="fs-3">Papa Perucha</h3>
-<p>Sector Gastronómico</p>
-			</div>
 
+			<h2>Sectores</h2>
+			<p>Los principales sectores que abarcamos son: Industrial, extractivo y servicios</p>
+			<div class="container">
+				<h3>Agroindustrias</h3>
+				<ul>
+					<li>Mango Congelado</li>
+					<li><strong>Ciruela de Hueso</strong> de la cual elaboramos mermeladas y conservas en diferentes presentaciones</li>
+					<li>Higo</li>
+					<li>Guayaba</li>
+				</ul>
+				<h3>Inmobiliaria</h3>
+				<h3>Servicios</h3>
+			</div>
 		</section>
 
 	</div>
