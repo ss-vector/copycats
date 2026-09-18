6755156f73ab87c7e1d09c6e9db8806a25acf8fa
ss-supernova91 <omar_mejia@hotmail.com>
2026-09-18 14:36:54 -0500

    fixing mistakes in sigle page

--- page.php
+++ page.php
@@ -17,7 +17,7 @@ get_header();
                 <article>
                     <?php if( have_posts() ):
                             while( have_posts() ): the_post(); ?>
-                                <h1><?php the_title( sprintf( '<a href="%s"><h2 class="blog-entry-title">', esc_url( get_permalink() ) ), '</h2></a>' ); ?></h1>
+                                <h1 class="blog-entry-title"><?php the_title(); ?></h1>
                                 <?php the_content(); ?>
                             <?php endwhile; ?>
                 <?php endif; ?>
